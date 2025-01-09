5bc61efe2125a9ae0bcb310f7766ba2d477d7863
Aurélien Brault <ahbrault@gmail.com>
2025-01-09 19:44:23 +0100

    Update sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,17 @@
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = 'https://www.joergleser.com';
+
     const routes = [
-        '',
+        '/en',
+        '/fr'
     ];
 
     return routes.map(route => ({
-        url: `https://www.joergleser.com${route}`,
+        url: `${baseUrl}${route}`,
         lastModified: new Date().toISOString(),
         changeFrequency: 'monthly' as const,
-        priority: 1,
+        priority: route === '/en' ? 1 : 0.9, // '/en' est prioritaire comme point d'entrée
     }));
 }
